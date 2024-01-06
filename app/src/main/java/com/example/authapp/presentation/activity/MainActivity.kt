216ec0345c374a5b49cd8ccdfd5577aff66a11e5
Dadah Taufik <dadahtaufikp14@gmail.com>
2024-01-06 08:26:41 +0700

    feature : add splash screen and navigation graph

--- app/src/main/java/com/example/authapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/authapp/presentation/activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.example.authapp
+package com.example.authapp.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.authapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/authapp/presentation/fragment/SplashFragment.kt
@@ -0,0 +1,63 @@
+package com.example.authapp.presentation.fragment
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.authapp.R
+import com.example.authapp.presentation.viewModel.SplashViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class SplashFragment : Fragment() {
+
+    private lateinit var viewModel: SplashViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val progressBar = view.findViewById<ProgressBar>(R.id.loadingProgressBar)
+        progressBar.visibility = View.VISIBLE
+
+        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+
+        // Pengecekan koneksi internet
+        if (isNetworkAvailable()) {
+            viewModel.handleSplashScreen {
+//                findNavController().navigate(R.id.)
+            }
+        } else {
+            showSnackbar("No internet access, please check")
+        }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+
+    private fun showSnackbar(message: String) {
+        val rootView: View = requireActivity().findViewById(android.R.id.content)
+        val snackbar = Snackbar.make(rootView, message, Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.black))
+        snackbar.show()
+    }
+}
